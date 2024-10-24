5e747d22ec08341ef3ce9ab53f0f8055a58fd035
Thejaswini-2178 <thejaswini2178@gmail.com>
2024-10-25 01:21:29 +0530

    Template-4

--- Template-4/template4/src/App.js
+++ Template-4/template4/src/App.js
@@ -1,15 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
-import Head4 from './Template-4/Head4';
-import Section from './Template-4/Section';
 import Footer from './Template-4/Footer';
+import Header4 from './Template-4/Header4';
+import Section from './Template-4/Section';
 
 function App() {
   return (
     <div className="App">
-      <Head4/>
+      <Header4/>
       <Section/>
       <Footer/>
+      
     </div>
   );
 }
