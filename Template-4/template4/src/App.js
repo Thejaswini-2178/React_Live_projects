dde27ac673b0e35c04425a2852b8a6ae05243aaa
Thejaswini-2178 <thejaswini2178@gmail.com>
2024-10-25 00:09:04 +0530

    Template-4

---
+++ Template-4/template4/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Head4 from './Template-4/Head4';
+import Section from './Template-4/Section';
+import Footer from './Template-4/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Head4/>
+      <Section/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Template-4/template4/src/Template-4/Footer.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+export class Footer extends Component {
+  render() {
+    return (
+      <div>
+{/* footer section */}
+<section className="footer_section">
+  <div className="container">
+    <p>
+      © <span id="displayYear" /> All Rights Reserved By
+      <a href="https://html.design/">Free Html Templates</a>
+    </p>
+  </div>
+</section>
+{/* footer section */}
+{/* jQery */}
+{/* popper js */}
+{/* bootstrap js */}
+{/* owl slider */}
+{/* custom js */}
+{/* Google Map */}
+{/* End Google Map */}
+
+      </div>
+    )
+  }
+}
+
+export default Footer
