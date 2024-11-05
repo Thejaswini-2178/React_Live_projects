a2bb9ba95bf6860a7723b40482c9eb7cb42a5b25
Thejaswini-2178 <thejaswini2178@gmail.com>
2024-11-05 07:33:38 +0530

    React-Converts-RentedCars-Templates

--- rentedcarsfor/src/App.js
+++ rentedcarsfor/src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import Aboutsection from './Renter cars-template/Aboutsection';
 import Bestsection from './Renter cars-template/Bestsection';
 import BlogSection from './Renter cars-template/7-Blog/BlogSection';
 import Booksection from './Renter cars-template/2-Book/Booksection';
@@ -8,9 +7,11 @@ import Clintsection from './Renter cars-template/9-Clint/Clintsection';
 import Contactsection from './Renter cars-template/10-Contact/Contactsection';
 import Contactus from './Renter cars-template/8-Contact/Contactus';
 import Footer from './Renter cars-template/12-Footer/Footer';
-import Header from './Renter cars-template/1-Header/Header';
 import Mapsection from './Renter cars-template/11-Map/Mapsection';
 import Rentsection from './Renter cars-template/6-Rent/Rentsection';
+import Header from './Renter cars-template/1-Header/Header';
+import Aboutsection from './Renter cars-template/4-ABOUT/Aboutsection';
+
 
 function App() {
   return (
