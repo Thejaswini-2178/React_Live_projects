416afa1d865525ab4f615dc84afe65572d7003eb
Thejaswini-2178 <thejaswini2178@gmail.com>
2024-11-06 22:34:24 +0530

    React-Converts-Retedcar-Template

--- rentedcarsfor/src/Renter cars-template/5-Best/Bestsection.jsx
+++ rentedcarsfor/src/Renter cars-template/5-Best/Bestsection.jsx
@@ -1,33 +1,28 @@
-import React, { Component } from 'react'
+import React from 'react';
+import data from './bestSectionData.json';
 
-export class Bestsection extends Component {
-  render() {
+const Bestsection = () => {
+    const { bestSection } = data;
+    const { container } = bestSection;
+    const { bookNow } = container;
+
     return (
-      <div>
-        {/* best section */}
-        <section className="best_section">
-          <div className="container">
-            <div className="book_now">
-              <div className="detail-box">
-                <h2>
-                  Our Best Cars
-                </h2>
-                <p>
-                  It is a long established fact that a reader will be distracted by the
-                </p>
-              </div>
-              <div className="btn-box">
-                <a href>
-                  Book Now
-                </a>
-              </div>
-            </div>
-          </div>
-        </section>
-        {/* end best section */}
-      </div>
-    )
-  }
-}
+        <div>
+            <section className={bestSection.className}>
+                <div className="container">
+                    <div className="book_now">
+                        <div className="detail-box">
+                            <h2>{bookNow.detail.title}</h2>
+                            <p>{bookNow.detail.description}</p>
+                        </div>
+                        <div className="btn-box">
+                            <a href={bookNow.button.href}>{bookNow.button.text}</a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
+};
 
-export default Bestsection
+export default Bestsection;
