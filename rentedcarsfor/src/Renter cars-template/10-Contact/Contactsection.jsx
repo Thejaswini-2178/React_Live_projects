685840375faa4230c29890cb67f345c854b55e02
Thejaswini-2178 <thejaswini2178@gmail.com>
2024-11-06 22:55:44 +0530

    React-Converts-Rented-Template

--- rentedcarsfor/src/Renter cars-template/10-Contact/Contactsection.jsx
+++ rentedcarsfor/src/Renter cars-template/10-Contact/Contactsection.jsx
@@ -1,101 +1,65 @@
-import React, { Component } from 'react'
+import React from 'react';
+import contactData from './contactSectionData.json';
 
-export class Contactsection extends Component {
-    render() {
-        return (
-            <div>
-                {/* contact section */}
-                <section className="contact_section layout_padding">
-                    <div className="container">
-                        <div className="heading_container">
-                            <h2>
-                                Request A call back
-                            </h2>
-                        </div>
-                        <div className="row">
-                            <div className="col-md-8 mx-auto">
-                                <div className="form_container">
-                                    <form>
-                                        <div className="form-row">
-                                            <div className="form-group col-md-6">
-                                                <input type="text" className="form-control" id="inputName4" placeholder="Name " />
+const Contactsection = () => {
+    const { contactSection } = contactData;
+    const { heading, form, contactItems, socialLinks } = contactSection;
+
+    return (
+        <div>
+            <section className="contact_section layout_padding">
+                <div className="container">
+                    <div className="heading_container">
+                        <h2>{heading.title}</h2>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-8 mx-auto">
+                            <div className="form_container">
+                                <form>
+                                    <div className="form-row">
+                                        {form.fields.map((field, index) => (
+                                            <div className={`form-group col-md-${field.type === "text" ? "6" : "12"}`} key={index}>
+                                                <input 
+                                                    type={field.type} 
+                                                    className="form-control" 
+                                                    id={field.id} 
+                                                    placeholder={field.placeholder} 
+                                                />
                                             </div>
-                                            <div className="form-group col-md-6">
-                                                <input type="text" className="form-control" id="inputSubject4" placeholder="Phone" />
-                                            </div>
-                                        </div>
-                                        <div className="form-row">
-                                            <div className="form-group col">
-                                                <input type="email" className="form-control" id="inputEmail4" placeholder="Email id" />
-                                            </div>
-                                        </div>
-                                        <div className="form-group">
-                                            <input type="text" className="form-control" id="inputMessage" placeholder="Message" />
-                                        </div>
-                                        <div className="d-flex justify-content-center">
-                                            <button type="submit" className>Send</button>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="contact_items">
-                            <a href>
-                                <div className="img-box">
-                                    <img src="images/location.png" alt />
-                                </div>
-                                <h6>
-                                    Loram Ipusum ari
-                                    lo elisant na
-                                </h6>
-                            </a>
-                            <a href>
-                                <div className="img-box">
-                                    <img src="images/call.png" alt />
-                                </div>
-                                <h6>
-                                    (+12 1234456789)
-                                </h6>
-                            </a>
-                            <a href>
-                                <div className="img-box">
-                                    <img src="images/mail.png" alt />
-                                </div>
-                                <h6>
-                                    demo@gmail.com
-                                </h6>
-                            </a>
-                        </div>
-                        <div className="social_container">
-                            <div className="social-box">
-                                <div>
-                                    <a href>
-                                        <img src="images/fb.png" alt />
-                                    </a>
-                                </div>
-                                <div>
-                                    <a href>
-                                        <img src="images/twitter.png" alt />
-                                    </a>
-                                </div>
-                                <div>
-                                    <a href>
-                                        <img src="images/linkedin.png" alt />
-                                    </a>
-                                </div>
-                                <div>
-                                    <a href>
-                                        <img src="images/insta.png" alt />
-                                    </a>
-                                </div>
+                                        ))}
+                                    </div>
+                                    <div className="d-flex justify-content-center">
+                                        <button type="submit" className="btn btn-primary">Send</button>
+                                    </div>
+                                </form>
                             </div>
                         </div>
                     </div>
-                </section>
-                {/* end contact section */}
-            </div>
-        )
-    }
-}
+                    <div className="contact_items">
+                        {contactItems.map((item, index) => (
+                            <a href="#" key={index}>
+                                <div className="img-box">
+                                    <img src={item.icon} alt="Contact Info" />
+                                </div>
+                                <h6>{item.info}</h6>
+                            </a>
+                        ))}
+                    </div>
+                    <div className="social_container">
+                        <div className="social-box">
+                            {socialLinks.map((link, index) => (
+                                <div key={index}>
+                                    <a href={link.link}>
+                                        <img src={link.icon} alt="Social Icon" />
+                                    </a>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
+};
 
-export default Contactsection
+export default Contactsection;
