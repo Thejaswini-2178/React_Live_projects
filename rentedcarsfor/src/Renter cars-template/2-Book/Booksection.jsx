ba2cce7f85f14780134eabdba32ac746d862f155
Thejaswini-2178 <thejaswini2178@gmail.com>
2024-11-06 22:33:27 +0530

    React-Converts-Retedcar-Template

--- rentedcarsfor/src/Renter cars-template/2-Book/Booksection.jsx
+++ rentedcarsfor/src/Renter cars-template/2-Book/Booksection.jsx
@@ -1,53 +1,46 @@
-import React, { Component } from 'react'
+import React from 'react';
+import data from './bookSectionData.json';
 
-export class Booksection extends Component {
-    render() {
-        return (
-            <div>
-                <section className="book_section">
-                    <div className="form_container">
-                        <form action>
-                            <div className="form-row">
-                                <div className="col-lg-8">
-                                    <div className="form-row">
-                                        <div className="col-md-6">
-                                            <label htmlFor="parkingName">Pick Up Locaion</label>
-                                            <input type="text" className="form-control" placeholder="acb " />
+const Booksection = () => {
+    const { bookSection } = data;
+    const { formContainer, imgBox } = bookSection;
+
+    return (
+        <div>
+            <section className={bookSection.className}>
+                <div className="form_container">
+                    <form action={formContainer.formAction}>
+                        <div className="form-row">
+                            <div className="col-lg-8">
+                                <div className="form-row">
+                                    {formContainer.formRows.map((row, index) => (
+                                        <div key={index} className={row.colClass}>
+                                            <label>{row.label}</label>
+                                            <input
+                                                type={row.input.type}
+                                                className={row.input.className}
+                                                placeholder={row.input.placeholder}
+                                            />
                                         </div>
-                                        <div className="col-md-6">
-                                            <label htmlFor="parkingNumber">Drop Location</label>
-                                            <input type="text" className="form-control" placeholder="acb " />
-                                        </div>
-                                    </div>
-                                    <div className="form-row">
-                                        <div className="col-md-6">
-                                            <label htmlFor="parkingName">Pick Up Date</label>
-                                            <input type="text" className="form-control" placeholder="07/09/2020" />
-                                        </div>
-                                        <div className="col-md-6">
-                                            <label htmlFor="parkingNumber">Return Date</label>
-                                            <input type="text" className="form-control" placeholder="07/09/2020" />
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col-lg-4">
-                                    <div className="btn-container">
-                                        <button type="submit" className>
-                                            Search
-                                        </button>
-                                    </div>
+                                    ))}
                                 </div>
                             </div>
-                        </form>
-                    </div>
-                    <div className="img-box">
-                        <img src="images/book-car.png" alt />
-                    </div>
-                </section>
-            </div>
-        )
-    }
-}
+                            <div className="col-lg-4">
+                                <div className="btn-container">
+                                    <button type={formContainer.button.type}>
+                                        {formContainer.button.text}
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <div className="img-box">
+                    <img src={imgBox.src} alt={imgBox.alt} />
+                </div>
+            </section>
+        </div>
+    );
+};
 
-
-export default Booksection
+export default Booksection;
