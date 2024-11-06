e40620ee66cb4c6a1513a5c9aa87e461b7bf1a98
Thejaswini-2178 <thejaswini2178@gmail.com>
2024-11-06 22:34:06 +0530

    React-Converts-Retedcar-Template

--- rentedcarsfor/src/Renter cars-template/4-ABOUT/Aboutsection.jsx
+++ rentedcarsfor/src/Renter cars-template/4-ABOUT/Aboutsection.jsx
@@ -1,34 +1,31 @@
-import React, { Component } from 'react'
+import React from 'react';
+import data from './aboutSectionData.json';
 
-export class Aboutsection extends Component {
-    render() {
-        return (
-            <section className="about_section layout_padding-bottom">
-                <div className="container-fluid">
-                    <div className="row">
-                        <div className="col-md-7 px-0">
-                            <div className="img-box">
-                                <img src="images/about-img.png" alt />
-                            </div>
+const Aboutsection = () => {
+    const { aboutSection } = data;
+    const { container } = aboutSection;
+    const { image, details } = container;
+
+    return (
+        <section className={aboutSection.className}>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-md-7 px-0">
+                        <div className="img-box">
+                            <img src={image.src} alt={image.alt} />
                         </div>
-                        <div className="col-md-4 col-lg-3">
-                            <div className="detail-box">
-                                <h2>
-                                    About Our Cars
-                                </h2>
-                                <p>
-                                    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem
-                                </p>
-                                <a href>
-                                    Read More
-                                </a>
-                            </div>
+                    </div>
+                    <div className="col-md-4 col-lg-3">
+                        <div className="detail-box">
+                            <h2>{details.title}</h2>
+                            <p>{details.description}</p>
+                            <a href={details.linkHref}>{details.linkText}</a>
                         </div>
                     </div>
                 </div>
-            </section>
-        )
-    }
-}
+            </div>
+        </section>
+    );
+};
 
-export default Aboutsection
+export default Aboutsection;
