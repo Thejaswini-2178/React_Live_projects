6c7c8f4a791ab8372bdf4fbbc6720e30c7f6c0aa
Thejaswini-2178 <thejaswini2178@gmail.com>
2024-11-06 22:05:07 +0530

    React-Template-Rentedcars

--- rentedcarsfor/src/Renter cars-template/1.1 Slider/SliderSection.jsx
+++ rentedcarsfor/src/Renter cars-template/1.1 Slider/SliderSection.jsx
@@ -1,65 +1,40 @@
-import React, { Component } from 'react'
-import 
+import React from 'react';
+import data from './sliderData.json';
 
-class SliderSection extends Component {
-    render() {
-        return (
-            <section className=" slider_section position-relative">
-                <div className="slider_container">
-                    <div className="img-box">
-                        <img src="images/hero-img.jpg" alt />
-                    </div>
-                    <div className="detail_container">
-                        <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
-                            <div className="carousel-inner">
-                                <div className="carousel-item active">
+const SliderSection = () => {
+    const { sliderSection } = data;
+    const { imgBox, detailContainer } = sliderSection.sliderContainer;
+    const { carousel } = detailContainer;
+
+    return (
+        <section className={sliderSection.className}>
+            <div className="slider_container">
+                <div className="img-box">
+                    <img src={imgBox.src} alt={imgBox.alt} />
+                </div>
+                <div className="detail_container">
+                    <div id={carousel.id} className="carousel slide" data-ride="carousel">
+                        <div className="carousel-inner">
+                            {carousel.slides.map((slide, index) => (
+                                <div key={index} className={`carousel-item ${index === 0 ? 'active' : ''}`}>
                                     <div className="detail-box">
-                                        <h1>
-                                           {SliderSection.head1.h1} 
-                                        </h1>
-                                        <a href>
-                                            Contact Us
-                                        </a>
+                                        <h1>{slide.title.split('\n').map((line, i) => <React.Fragment key={i}>{line}<br /></React.Fragment>)}</h1>
+                                        <a href={slide.linkHref}>{slide.linkText}</a>
                                     </div>
                                 </div>
-                                <div className="carousel-item">
-                                    <div className="detail-box">
-                                        <h1>
-                                            Rent Car <br />
-                                            Experts <br />
-                                            Service
-                                        </h1>
-                                        <a href>
-                                            Contact Us
-                                        </a>
-                                    </div>
-                                </div>
-                                <div className="carousel-item">
-                                    <div className="detail-box">
-                                        <h1>
-                                            Rent Car <br />
-                                            Experts <br />
-                                            Service
-                                        </h1>
-                                        <a href>
-                                            Contact Us
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                            <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                                <span className="sr-only">{SliderSection.a3}</span>
-                            </a>
-                            <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                                <span className="sr-only">{SliderSection.a4}</span>
-                            </a>
+                            ))}
                         </div>
+                        <a className="carousel-control-prev" href={`#${carousel.id}`} role="button" data-slide="prev">
+                            <span className="sr-only">Previous</span>
+                        </a>
+                        <a className="carousel-control-next" href={`#${carousel.id}`} role="button" data-slide="next">
+                            <span className="sr-only">Next</span>
+                        </a>
                     </div>
                 </div>
-            </section>
-
-        )
-    }
+            </div>
+        </section>
+    );
 }
 
-export default SliderSection
+export default SliderSection;
