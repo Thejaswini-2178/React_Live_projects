8ab7909b7ee7a5a88e631ccecba561735c351b93
Thejaswini-2178 <thejaswini2178@gmail.com>
2024-11-07 18:37:04 +0530

    React-Create-Template

--- rentedcarsfor/src/Renter cars-template/7-Blog/BlogSection.jsx
+++ rentedcarsfor/src/Renter cars-template/7-Blog/BlogSection.jsx
@@ -1,112 +1,47 @@
-import React, { Component } from 'react'
+import React from 'react';
+import data from './blogSectionData.json';
 
-export class BlogSection extends Component {
-  render() {
+const BlogSection = () => {
+    const { blogSection } = data;
+    const { container } = blogSection;
+    const { heading, blogs } = container;
+
     return (
-      <div>
         <div>
-          {/* blog section */}
-          <section className="blog_section layout_padding">
-            <div className="container">
-              <div className="heading_container">
-                <h2>
-                  Our Latest Blog
-                </h2>
-                <p>
-                  It is a long established fact that a reader will be distracted by the
-                </p>
-              </div>
-            </div>
-            <div className="blog_container">
-              <div className="layout_padding2-top">
-                <div className="carousel-wrap ">
-                  <div className="owl-carousel">
-                    <div className="item">
-                      <div className="box">
-                        <div className="date-box">
-                          <h6>
-                            04 Nov 2019
-                          </h6>
-                        </div>
-                        <div className="img-box">
-                          <img src="images/b-1.jpg" alt="Motercars" />
-                        </div>
-                        <div className="detail-box">
-                          <h5>
-                            Vintage
-                          </h5>
-                          <p>
-                            It is a long established fact that a reader will be distracted by the readable The point of using Lorem </p>
-                        </div>
-                      </div>
+            <section className={blogSection.className}>
+                <div className="container">
+                    <div className="heading_container">
+                        <h2>{heading.title}</h2>
+                        <p>{heading.description}</p>
                     </div>
-                    <div className="item">
-                      <div className="box">
-                        <div className="date-box">
-                          <h6>
-                            04 Nov 2019
-                          </h6>
-                        </div>
-                        <div className="img-box">
-                          <img src="images/b-2.jpg" alt="Motercars" />
-                        </div>
-                        <div className="detail-box">
-                          <h5>
-                            Steering wheels
-                          </h5>
-                          <p>
-                            It is a long established fact that a reader will be distracted by the readable The point of using Lorem </p>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="item">
-                      <div className="box">
-                        <div className="date-box">
-                          <h6>
-                            04 Nov 2019
-                          </h6>
-                        </div>
-                        <div className="img-box">
-                          <img src="images/b-3.jpg" alt="Motercars" />
-                        </div>
-                        <div className="detail-box">
-                          <h5>
-                            Buick Car
-                          </h5>
-                          <p>
-                            It is a long established fact that a reader will be distracted by the readable The point of using Lorem </p>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="item">
-                      <div className="box">
-                        <div className="date-box">
-                          <h6>
-                            04 Nov 2019
-                          </h6>
-                        </div>
-                        <div className="img-box">
-                          <img src="images/b-2.jpg" alt="Motercars" />
-                        </div>
-                        <div className="detail-box">
-                          <h5>
-                            Steering wheels
-                          </h5>
-                          <p>
-                            It is a long established fact that a reader will be distracted by the readable The point of using Lorem </p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
                 </div>
-              </div>
-            </div>
-          </section>
-          {/* end blog section */}
+                <div className="blog_container">
+                    <div className="layout_padding2-top">
+                        <div className="carousel-wrap">
+                            <div className="owl-carousel">
+                                {blogs.map((blog, index) => (
+                                    <div key={index} className="item">
+                                        <div className="box">
+                                            <div className="date-box">
+                                                <h6>{blog.date}</h6>
+                                            </div>
+                                            <div className="img-box">
+                                                <img src={blog.image.src} alt={blog.image.alt} />
+                                            </div>
+                                            <div className="detail-box">
+                                                <h5>{blog.title}</h5>
+                                                <p>{blog.description}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </div>
-      </div>
-    )
-  }
-}
+    );
+};
 
-export default BlogSection
+export default BlogSection;
