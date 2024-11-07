ba31c23a1e279fcb6f7a64a6e55e0ca3abaefa19
Thejaswini-2178 <thejaswini2178@gmail.com>
2024-11-07 18:36:49 +0530

    React-Create-Template

--- rentedcarsfor/src/Renter cars-template/6-Rent/Rentsection.jsx
+++ rentedcarsfor/src/Renter cars-template/6-Rent/Rentsection.jsx
@@ -1,85 +1,34 @@
-import React, { Component } from 'react'
+import React from 'react';
+import data from './rentSectionData.json';
 
-export class Rentsection extends Component {
-    render() {
-        return (
-            <div>
-                {/* rent section */}
-                <section className="rent_section layout_padding">
-                    <div className="container">
-                        <div className="rent_container">
-                            <div className="box">
+const Rentsection = () => {
+    const { rentSection } = data;
+    const { container } = rentSection;
+    const { rentContainer, seeMoreButton } = container;
+
+    return (
+        <div>
+            <section className={rentSection.className}>
+                <div className="container">
+                    <div className="rent_container">
+                        {rentContainer.map((item, index) => (
+                            <div key={index} className="box">
                                 <div className="img-box">
-                                    <img src="images/r-1.png" alt="Cars" />
+                                    <img src={item.image.src} alt={item.image.alt} />
                                 </div>
                                 <div className="price">
-                                    <a href>
-                                        Rent $200
-                                    </a>
+                                    <a href={item.linkHref}>{item.price}</a>
                                 </div>
                             </div>
-                            <div className="box">
-                                <div className="img-box">
-                                    <img src="images/r-2.png" alt="Cars" />
-                                </div>
-                                <div className="price">
-                                    <a href>
-                                        Rent $200
-                                    </a>
-                                </div>
-                            </div>
-                            <div className="box">
-                                <div className="img-box">
-                                    <img src="images/r-3.png" alt="Cars" />
-                                </div>
-                                <div className="price">
-                                    <a href>
-                                        Rent $200
-                                    </a>
-                                </div>
-                            </div>
-                            <div className="box">
-                                <div className="img-box">
-                                    <img src="images/r-4.png" alt="Cars" />
-                                </div>
-                                <div className="price">
-                                    <a href>
-                                        Rent $200
-                                    </a>
-                                </div>
-                            </div>
-                            <div className="box">
-                                <div className="img-box">
-                                    <img src="images/r-5.png" alt="Cars" />
-                                </div>
-                                <div className="price">
-                                    <a href>
-                                        Rent $200
-                                    </a>
-                                </div>
-                            </div>
-                            <div className="box">
-                                <div className="img-box">
-                                    <img src="images/r-6.png" alt="Cars" />
-                                </div>
-                                <div className="price">
-                                    <a href>
-                                        Rent $200
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="btn-box">
-                            <a href>
-                                See More
-                            </a>
-                        </div>
+                        ))}
                     </div>
-                </section>
-                {/* end rent section */}
-            </div>
-        )
-    }
-}
+                    <div className="btn-box">
+                        <a href={seeMoreButton.href}>{seeMoreButton.text}</a>
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
+};
 
-export default Rentsection
+export default Rentsection;
